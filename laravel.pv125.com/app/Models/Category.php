7ba954b418dd916ba456486d7e5b7755d9c4420a
novakvova <novakvova@gmail>
2023-06-23 19:21:49 +0300

    Begin working Product images

--- laravel.pv125.com/app/Models/Category.php
+++ laravel.pv125.com/app/Models/Category.php
@@ -14,4 +14,11 @@ class Category extends Model
       'image',
       'description'
     ];
+    /**
+     * Get the products for the category.
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
---
+++ laravel.pv125.com/app/Models/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'price',
+        'description'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Get the product_images for the product.
+     */
+    public function product_images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+}
